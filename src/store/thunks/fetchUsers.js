da19cb93f92763d6020a71a4b68c45b52c691b94
1001hadi <hadi.jaf.1001@gmail.com>
2024-01-08 14:15:57 -0700

    test function has been commented

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -4,15 +4,15 @@ import axios from "axios";
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get("http://localhost:3005/users");
   // Only for Development porpose
-  await pause(2000);
+  //   await pause(2000);
   return response.data;
 });
 
-// Only for Development porpose
-const pause = (duration) => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, duration);
-  });
-};
+// // Only for Development porpose
+// const pause = (duration) => {
+//   return new Promise((resolve) => {
+//     setTimeout(resolve, duration);
+//   });
+// };
 
 export { fetchUsers };
