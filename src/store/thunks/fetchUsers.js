07c4afda1772dde663468f374d4c779de62ef027
1001hadi <hadi.jaf.1001@gmail.com>
2024-01-10 10:02:19 -0700

    adding user functionallity has been added

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -2,9 +2,9 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
-  const response = await axios.get("http://localhost:3005/users");
+  const response = await axios.get("http://localhost:5000/users");
   // Only for Development porpose
-  //   await pause(2000);
+  // await pause(2000);
   return response.data;
 });
 
